f33dba8a3ed99ecb59fb52a9deb4cb148589b9b6
weidu0208 <wedu@ucdavis.edu>
2021-03-06 13:49:26 -0800

    log(1-Nmin/max(Nmin)) VS Time

--- PlotData.R
+++ PlotData.R
@@ -2,9 +2,12 @@ setwd("E:/E-BOOK/STA 260/")
 library(ggplot2)
 library(dplyr)
 library(stringr)
+library(lme4)
+library(ggthemes)
 
 data = read.csv("Compost_StatsClass_021521[26705].csv")
 data = filter(data, N.min.amd.pct >= 0)
+data$CNratio = as.numeric(str_extract_all(data$Soil,"[0-9.]+[0-9]",simplify = TRUE))
 
 #Each curve is specified by Curve_ID, Amendment, Soil, Temperature and WHC
 #New curve ID is IDD
@@ -53,12 +56,15 @@ df_clean = df %>%
            (IDD == 43 & round(Time,1) %in% c(13.9))|
            (IDD == 44 & round(Time,2) %in% c(1.43))|
            (IDD == 45 & round(Time,2) %in% c(13.86))|
+           (IDD == 55 & Time %in% c(16))|
            (IDD == 58 & Time %in% c(8))|
            (IDD == 80 & Time %in% c(12)) |
            (IDD == 81 & Time %in% c(12)) |
            (IDD == 89 & Time %in% c(3)) | 
            (IDD == 90 & Time %in% c(1)) |
-           (IDD == 91 & Time %in% c(14))) )
+           (IDD == 91 & Time %in% c(14))| 
+           (IDD %in% c(16,50,41,42,44,64,72,75)))) %>%
+  filter(n()>3)
 
 #discuss?
 c(16, 41, 50, 64, 72,75)
@@ -71,37 +77,58 @@ for(i in unique(df_clean$IDD)){
   df_i = df_i[order(df_i$Time),]
   Nim = df_i$N.min.amd.pct
   t = df_i$Time
-  opt = optim(c(Nim[length(t)],1),loss)
+  opt = optim(c(max(Nim),1),loss)
   N0k = rbind(N0k,opt$par)
-  #pp <- ggplot() + 
-  #  xlim(0, max(t))+ 
-  #  geom_function(fun = fn(opt$par)) + 
-  #  geom_point(aes(x=t,y=Nim),col = "blue") + 
-  #  ggtitle(paste("IDD",df_i$IDD,(df_i$Amendment),df_i$Soil,"TEMP",df_i$Temperature,"WHC",df_i$WHC))+
-  #  ylab("Nmin")
-  #print(pp)
 }
-
-
-#organized data(N0 & k included): nSamples is number of samples in the curve
-aggregate(numeric(nrow(df_clean)),df_clean[c("IDD")],length)
 colnames(N0k) = c("N0","k")
+#organized data(N0 & k included): nSamples is number of samples in the curve
 df_Nk = cbind(df_clean[!duplicated(df_clean[c("Temperature","WHC","IDD")]),][c(1:5,8:9)],N0k)
-df_NK = df_Nk[c("ID","Curve_ID","IDD","Temperature","WHC","Amendment","Soil","N0","k")]
 df_Nk$nSamples = aggregate(numeric(nrow(df_clean)),df_clean[c("IDD")],length)[,2]
+#merge df_clean and df_Nk
+df_merge = df_clean %>%
+  merge(df_Nk) %>%
+  mutate(Nmin.Nmax = N.min.amd.pct/(max(N.min.amd.pct)+0.05)) %>%
+  mutate(across(is.character, as.factor))
+df_merge$Temperature =factor(df_merge$Temperature)
+df_merge$WHC =factor(df_merge$WHC)
+df_merge$ID = factor(df_merge$ID)
+df_merge$Curve_ID = factor(df_merge$Curve_ID)
+df_merge$CNratio = factor(df_merge$CNratio)
 
-#Relationships between T and k
-plot(df_Nk$Temperature,log(df_Nk$k))
-plot(df_Nk$Temperature,(df_Nk$k))
-boxplot(log(df_Nk$k)~df_Nk$Temperature)
+#Time VS log(1-Nmin/max(Nmin)) for each IDD and grouped by Temprature/WHC/CNratio/Soil/Amendments/ID/Curve_ID
+ggplot(data = df_merge,aes(Time,log(1-Nmin.Nmax)))+
+  geom_line(aes(colour = ID,group=IDD),size = 1)+
+  geom_point(aes(colour = ID,group=IDD),size = 1)
+#Temperature 23, 25, 30
+df_merge_23.25.30 = filter(df_merge,Temperature %in% c(23,25,30))
+ggplot(data = df_merge_23.25.30,aes(Time,log(1-Nmin.Nmax)))+
+  geom_line(aes(colour = Temperature,group=IDD),size = 1,alpha = 0.5)+
+  geom_point(aes(colour = Temperature,group=IDD),size = 1)
 
-#Relationships between WHC and k
-plot(df_Nk$WHC,log(df_Nk$k))
-plot(df_Nk$WHC,(df_Nk$k))
+#Scatter plot of Temperature VS log(k_hat)
+plot((df_Nk$Temperature),log(df_Nk$k),xlab = "Temp",ylab = "k_hat")
+#Scatter plot of CNratio VS log(k_hat)
+plot(round(df_Nk$CNratio,1),log(df_Nk$k),xlab = "CNratio",ylab = "k_hat")
+#Scatter plot of WHC VS log(k_hat)
+plot(df_Nk$WHC,log(df_Nk$k),xlab = "WHC",ylab = "k_hat")
+#Scatter plot of CNratio VS N0 
+plot(round(df_Nk$CNratio,1),log(df_Nk$N0),xlab = "CNratio",ylab = "N0")
+boxplot(df_Nk$N0~df_Nk$CNratio)
+
+
+#anova model: k_hat ~ Temperature*WHC + error:
+fit_aov.TW = aov(log(k)~Temperature*WHC,df_Nk)
+summary(fit_aov.TW)
+
+#fit lmm model: k_hat ~ Temperature + WHC + 1|Soil + error
+fit_lmm = lmer(log(k)~ WHC + Temperature-1+(1|Soil), df_Nk)
+summary(fit_lmm)
+
+
 
 
 #plot for certain group
-for(i in unique(df_clean$IDD[which(df_clean$Temperature == 25)])){
+for(i in unique(df_clean$IDD)){
   df_i = df_clean[which(df_clean$IDD ==i),]
   df_i = df_i[order(df_i$Time),]
   Nim = df_i$N.min.amd.pct
@@ -115,19 +142,7 @@ for(i in unique(df_clean$IDD[which(df_clean$Temperature == 25)])){
   print(pp)
 }
 
-#Temp 15: IDD 44 seems missing points between time 0 and time 5 --> extreme large k
 
-#Temp 23: IDD 64 72 75 ? 
-
-test = filter(df_Nk, !IDD %in% c(44,64,72,75))
-test$CNratio = as.numeric(str_extract_all(test$Soil,"[0-9.]+[0-9]",simplify = TRUE))
-
-plot(test$Temperature,log(test$k))
-boxplot(log(test$k)~test$Amendment)
-plot(test$CNratio,log(test$k))
-plot(test$CNratio,(test$k))
-plot(test$CNratio,log(test$N0))
-boxplot(log(test$k)~test$CNratio)
 
 
 
